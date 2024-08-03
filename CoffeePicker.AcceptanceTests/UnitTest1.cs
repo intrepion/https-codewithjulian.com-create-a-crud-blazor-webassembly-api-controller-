e02cd5866589054fc55b05b0aa7494348cac8f50
Oliver Forral <intrepion@gmail.com>
2024-08-03 14:45:52 -0600

    dotnet new nunit --name CoffeePicker.AcceptanceTests

---
+++ CoffeePicker.AcceptanceTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace CoffeePicker.AcceptanceTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
